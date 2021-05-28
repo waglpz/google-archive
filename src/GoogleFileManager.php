191eaeecb8ef21328f3c4a8ff06ec6f97806c058
Lutz Krüger <lutz.krueger@veolia.com>
2021-05-28 14:34:53 +0200

    chore: Google File Mannager fertiggestellt

--- src/GoogleFileManager.php
+++ src/GoogleFileManager.php
@@ -6,6 +6,8 @@ namespace Waglpz\GcloudArchiv;
 
 use Google\Cloud\Storage\StorageClient;
 use Google\Cloud\Storage\StorageObject;
+use Psr\Http\Message\StreamInterface;
+use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
@@ -13,11 +15,13 @@ final class GoogleFileManager implements GoogleFileStorage
 {
     private StorageClient $googleStorageClient;
     private string $bucketName;
+    private LoggerInterface $logger;
 
-    public function __construct(StorageClient $googleStorageClient, string $bucketName)
+    public function __construct(StorageClient $googleStorageClient, string $bucketName, LoggerInterface $logger)
     {
         $this->googleStorageClient = $googleStorageClient;
         $this->bucketName          = $bucketName;
+        $this->logger              = $logger;
     }
 
     public function fromFile(string $filename): UuidInterface
@@ -25,28 +29,44 @@ final class GoogleFileManager implements GoogleFileStorage
         $bucket = $this->googleStorageClient->bucket($this->bucketName);
 
         if (\file_exists($filename) || \is_readable($filename)) {
-            $uuid = Uuid::uuid4();
-            $bucket->upload(\fopen($filename, 'rb'), ['name' => $uuid->toString()]);
+            $uuid        = Uuid::uuid4();
+            $fileHandler = \fopen($filename, 'rb');
+
+            if (! \is_resource($fileHandler)) {
+                $this->logger->error('File is invalid.');
+
+                throw new \RuntimeException('File is invalid.');
+            }
+
+            $bucket->upload($fileHandler, ['name' => $uuid->toString()]);
+            $this->logger->debug('File was uploaded');
 
             return $uuid;
         }
 
-        throw new \RuntimeException('File ' . $filename . ' does not exist or not readable.');
+        $message = 'File ' . $filename . ' does not exist or not readable.';
+        $this->logger->error($message);
+
+        throw new \RuntimeException($message);
     }
 
     public function fromBase64(string $content): UuidInterface
     {
         $bucket = $this->googleStorageClient->bucket($this->bucketName);
 
-        $binary = \base64_decode($content, false);
+        $binary = \base64_decode($content, true);
         if ($binary !== false) {
             $uuid = Uuid::uuid4();
             $bucket->upload($binary, ['name' => $uuid->toString()]);
+            $this->logger->debug('File was uploaded');
 
             return $uuid;
         }
 
-        throw new \RuntimeException('Base 64 content is not properly decoded.');
+        $message = 'Base 64 content is not properly decoded.';
+        $this->logger->error($message);
+
+        throw new \RuntimeException($message);
     }
 
     public function object(UuidInterface $name): StorageObject
@@ -56,10 +76,32 @@ final class GoogleFileManager implements GoogleFileStorage
         return $bucket->object($name->toString());
     }
 
-    public function delete(\Ramsey\Uuid\UuidInterface $uuid): void
+    public function delete(UuidInterface $uuid): void
     {
         $bucket = $this->googleStorageClient->bucket($this->bucketName);
 
+        $this->logger->debug('Try to delete a File');
         $bucket->object($uuid->toString())->delete();
+    }
+
+    public function getByName(UuidInterface $googleFileName): StreamInterface
+    {
+        $bucket = $this->googleStorageClient->bucket($this->bucketName);
+
+        $fileObject = $bucket->object($googleFileName->toString());
+
+        if ($fileObject->exists()) {
+            return $fileObject->downloadAsStream();
+        }
+
+        $message =  \sprintf(
+            'File name is invalid given "%s" or File does not exista in bucket "%s".',
+            $googleFileName->toString(),
+            $this->bucketName
+        );
+
+        $this->logger->error($message);
+
+        throw new \InvalidArgumentException($message);
     }
 }
--- src/GoogleFileStorage.php
+++ src/GoogleFileStorage.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace Waglpz\GcloudArchiv;
 
+use Psr\Http\Message\StreamInterface;
+use Ramsey\Uuid\UuidInterface;
+
 interface GoogleFileStorage
 {
-    public function fromFile(string $filename): \Ramsey\Uuid\UuidInterface;
+    public function fromFile(string $filename): UuidInterface;
 
-    public function fromBase64(string $content): \Ramsey\Uuid\UuidInterface;
+    public function fromBase64(string $content): UuidInterface;
 
-    public function delete(\Ramsey\Uuid\UuidInterface $uuid): void;
+    public function delete(UuidInterface $uuid): void;
+
+    public function getByName(UuidInterface $googleFileName): StreamInterface;
 }
