0a3e559e4e25b235ec75dbd62c9c06dbef9b1e52
Lutz Krüger <lutz.krueger@veolia.com>
2021-05-28 14:35:16 +0200

    chore: Beispielprogramm angepaßt

--- examples/simple-example.php
+++ examples/simple-example.php
@@ -2,21 +2,60 @@
 
 declare(strict_types=1);
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogHandler;
+use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
+
 include 'vendor/autoload.php';
 
 $config = [
     'projectId'   => 'de-ist-vwd-datadesk',
     'keyFilePath' => __DIR__ . '/cred.json',
 ];
+
+$requiredFields = [
+    'a' => true,
+    'z' => true,
+];
+
+$loggerFactory = new \MonologFactory\LoggerFactory();
+
+$logger = $loggerFactory->create('my_logger', [
+    'handlers'   => [
+        [
+            'name'         => SyslogHandler::class,
+            'params'       => [
+                'ident'    => 'onlinemsg',
+                'facility' => 'local0',
+                'level'    => Logger::DEBUG,
+            ],
+        ],
+        [
+            'name'       => StreamHandler::class,
+            'params'     => [
+                'stream' => 'php://STDOUT',
+                'level'  => Logger::DEBUG,
+            ],
+        ],
+    ],
+    'processors' => [
+        [
+            'name' => PsrLogMessageProcessor::class,
+        ],
+    ],
+]);
+
 $gcStorageClient = new \Google\Cloud\Storage\StorageClient($config);
 $bucketName = 's3_test_bucket';
 $gcFileManager = new \Waglpz\GcloudArchiv\GoogleFileManager($gcStorageClient, $bucketName);
 $fireStoreClient = new \Google\Cloud\Firestore\FirestoreClient($config);
 
-$archivator = new \Waglpz\GcloudArchiv\Archivator($fireStoreClient, $gcFileManager);
+$archivator = new \Waglpz\GcloudArchiv\Archivator($fireStoreClient, $fireStoreClient, $gcFileManager, $requiredFields);
 
 $content = \base64_encode('Das ist Ein Test');
-$ergebniss = $archivator->putFile($content, 'Simple example test', '1234567890');
+$ergebniss = $archivator->putFile($content, 'Simple example test', '1234567890', 'a:AAA', 'b:YYY', 'z:ZZZ');
+
 
 if ($ergebniss === true) {
     echo "file was uploaded ";
@@ -24,7 +63,10 @@ if ($ergebniss === true) {
     echo "File was not uploaded :(";
 }
 
-// todo implement
-// $datei = $archivator->getFile('Simple example test', '1234567890');
-
 echo PHP_EOL;
+
+$content = $archivator->downloadAsStream('Simple example test', '1234567890')->getContents();
+
+shell_exec("echo " . $content . " > ./1234567890.pdf");
+
+
